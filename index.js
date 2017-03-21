857d3ffb5f5e12d24738f48e619bcc98247bf12f
Petko D. Petkov <pdp@websecurify.com>
2017-03-21 17:09:09 +0000

    Added loadable modules and the modules command

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const path = require('path')
 const yargs = require('yargs')
 const pownModules = require('pown-modules')
 
-const main = (commands) => {
-    let y = yargs.usage(`Usage: $0 <command> [options]`)
+const main = (modules, commands) => {
+    let y = yargs.usage(`Usage: $0 [options] <command> [command options]`)
+
+    y = y.command({
+        command: 'modules',
+        describe: 'List loadable modules',
+
+        builder: {
+        },
+
+        handler: (argv) => {
+            Object.keys(modules).forEach(module => console.log(module, '-', module.desc || module.describe || module.description || ''))
+        }
+    })
+
+    y = y.options('modules', {
+        alias: 'm',
+        type: 'string',
+        describe: 'Load modules'
+    })
 
     commands.forEach(command => {
         const module = require(command)
@@ -17,23 +35,49 @@ const main = (commands) => {
 
     y = y.demandCommand(1, 'You need to specify a command').help()
 
-    y.argv
+    const argv = y.argv
+
+    if (argv.modules) {
+        argv.modules.split(',').forEach((name) => {
+            name = name.trim()
+
+            if (name === '*') {
+                Object.values(modules).forEach(module => require(module))
+            } else {
+                const module = modules[name]
+
+                if (module) {
+                    require(module)
+                } else {
+                    y = y.epilog(`Unrecognized module ${name}.`)
+                    y = y.showHelp()
+
+                    process.exit(1)
+                }
+            }
+        })
+    }
 }
 
 const boot = (modules) => {
-    let commands = []
+    let loadableModules = {}
+    let loadableCommands = []
 
     modules.forEach((module) => {
-        const moduleCommands = module.config.commands
-
-        if (!moduleCommands) {
-            return
+        if (module.config.main) {
+            loadableModules[module.package.name] = path.resolve(module.realpath, module.config.main)
         }
 
-        commands = commands.concat(moduleCommands.map(command => path.resolve(module.realpath, command)))
+        if (module.config.command) {
+            loadableCommands = loadableCommands.concat([path.resolve(module.realpath, module.config.command)])
+        }
+
+        if (module.config.commands) {
+            loadableCommands = loadableCommands.concat(module.config.commands.map(command => path.resolve(module.realpath, command)))
+        }
     })
 
-    main(commands)
+    main(loadableModules, loadableCommands)
 }
 
 pownModules.list((err, modules) => {
