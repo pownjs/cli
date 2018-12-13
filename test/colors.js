59a143aa3ef59e46114456f9feb117ea91f76d87
Petko D. Petkov <pdp@gnucitizen.org>
2018-12-13 11:02:40 +0000

    Improved tests.

--- test/colors.js
+++ test/colors.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const colors = require('../lib/colors')
 
 describe('colors', () => {
-    it('should produce', () => {
+    it('should color', () => {
         assert(colors.red('test').length > 0)
     })
 })
