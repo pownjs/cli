d0388a2a792d0e784a161fb8784889250fd411c4
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-30 08:46:24 +0000

    Better cli module load.

--- bin/cli.js
+++ bin/cli.js
@@ -1,40 +1,29 @@
 #!/usr/bin/env node
 
-const path = require('path')
 const modules = require('@pown/modules')
 
 const colors = require('../lib/colors')
 const { execute } = require('../lib/cli')
 
-const boot = async(modules) => {
-    let loadableModules = {}
-    let loadableCommands = []
-
-    modules.forEach((module) => {
-        if (module.config.main) {
-            loadableModules[module.package.name] = path.resolve(module.realpath, module.config.main)
-        }
-
-        if (module.config.command) {
-            loadableCommands = loadableCommands.concat([path.resolve(module.realpath, module.config.command)])
-        }
-
-        if (module.config.commands) {
-            loadableCommands = loadableCommands.concat(module.config.commands.map(command => path.resolve(module.realpath, command)))
-        }
-    })
-
+const boot = async({ loadableModules, loadableCommands }) => {
     const log = console.log.bind(console)
 
     console.info = function(...args) {
+        // NOTE: should we handle multiline
+
         log(colors.green('*'), ...args)
     }
 
     console.warn = function(...args) {
+        // NOTE: should we handle multiline
+
         log(colors.yellow('!'), ...args)
     }
 
     console.error = function(...args) {
+        // NOTE: should we handle multiline
+        // NOTE: will effect yargs usage output
+
         log(colors.red('x'), ...(process.env.DEBUG ? args : args.map((error) => {
             return error && error.message ? error.message : error
         })))
@@ -43,7 +32,7 @@ const boot = async(modules) => {
     await execute(process.argv.slice(2), { loadableModules, loadableCommands })
 }
 
-modules.list(async(err, modules) => {
+modules.extract(async(err, modules) => {
     if (err) {
         console.error(err)
     }
